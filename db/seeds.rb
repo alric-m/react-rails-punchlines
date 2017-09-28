83e3186cf1e134618d1826384898d8a46cbb6884
Matthias <matthias.alric@toxicode.fr>
2017-09-28 23:33:07 +0200

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,33 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'net/http'
 
+puts "Delete all punchlines!"
 Punchline.delete_all
+puts "Done!"
 
-Punchline.create! (
-  [
-    {
-      text: "Be yourself; everyone else is already taken.",
-      author: "Oscar Wilde"
-    },
-    {
-      text: "Two things are infinite: the universe and human stupidity; " \
-            "and I'm not sure about the universe.",
-      author: "Albert Einstein"
-    },
-    {
-      text: "So many books, so little time.",
-      author: "Frank Zappa"
-    },
-    {
-      text: "Be the change that you wish to see in the world",
-      author: "Mahatma Gandhi"
-    },
-    {
-      text: "If you tell the truth, you don't have to remember anything.",
-      author: "Mark Twain"
-    }
-  ]
-)
+puts "Fetching punchline json for seeds from https://gist.githubusercontent.com/lambda2/cb88d3c23475f2db1cc96bdcdee4f02c/raw/bb4acc4b68d854ec7444fa8925bc88d1de375bb4/punchlines.json (thanks a lot lambda2 !)"
+source = "https://gist.githubusercontent.com/lambda2/cb88d3c23475f2db1cc96bdcdee4f02c/raw/bb4acc4b68d854ec7444fa8925bc88d1de375bb4/punchlines.json"
+resp = Net::HTTP.get_response(URI.parse(source))
+data = resp.body
+results = JSON.parse(data)
+
+puts "Formatting results for our database!"
+results.each do |punchline_params|
+  # we don't need tags
+  punchline_params.delete("tags")
+
+  # rename key according our data model
+  punchline_params["text"] = punchline_params["content"].gsub("« ", "").gsub(" »", "")
+  punchline_params.delete("content")
+
+  punchline_params["song"] = punchline_params["title"]
+  punchline_params.delete("title")
+end
+puts "Done!"
+
+puts "Seeding..."
+Punchline.create! results
 
 puts "Punchlines seeded!"
+puts "#{Punchline.count} punchlines!"
